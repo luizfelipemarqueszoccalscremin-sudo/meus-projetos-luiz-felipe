b621125bc73a4f49fabf35acfda3118550d2a776
luizfelipemarqueszoccalscremin-sudo <luizfelipemarqueszoccalscremin@gmail.com>
2026-06-05 00:29:28 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+print("--- CALCULADORA DE SOMA ---")
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é {soma}")
