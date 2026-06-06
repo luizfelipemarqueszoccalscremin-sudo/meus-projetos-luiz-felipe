f14395a1608a7ad534774226d973ab5eade1a9fc
luizfelipemarqueszoccalscremin-sudo <luizfelipemarqueszoccalscremin@gmail.com>
2026-06-06 00:03:50 -0300

    Add files via upload

---
+++ decisao.py
@@ -0,0 +1,6 @@
+print("--- TESTE DE DECISÃO ---")
+idade = int(input ("digite a sua idade: "))
+if idade >= 18:
+    print("você e maior de idade")
+else:  print("você e menor de idade")
+print("fim do programa")
